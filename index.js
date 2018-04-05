b54ac76aa41854c9cc8f56c938b6af61c4e85071
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-05 16:15:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 
 const returnFirstTwoDrivers = function(drivers) {
   return drivers.slice().splice(0, 2);
@@ -23,3 +24,8 @@ const fareTripler = createFareMultiplier(3);
 const selectDifferentDrivers = function (arrayOfDrivers, driverSelector) {
   return driverSelector(arrayOfDrivers);
 };
+=======
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(2)
+};
+>>>>>>> 167d54a1ec9b916290f731957ea489509e923498
